2ecfe9caefd8b5dc535bdb930d49ac43d854985f
garwynn <byon425@yahoo.com>
2013-02-13 01:24:32 -0600

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 28 15:41:37 KST 2013"
+#define LINUX_COMPILE_BY "jryukim"
+#define LINUX_COMPILE_HOST "SystemSW-jryukim-BuildPC"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
